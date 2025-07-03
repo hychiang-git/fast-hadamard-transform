dc4a3ff24113196951022dab569091a5c3d02caa
Chiang Hung-Yueh (hc29225) <hc29225@austin.utexas.edu@lovelace.austin.utexas.edu>
2025-07-03 16:52:51 -0500

    add 84N and 172N

--- csrc/fast_hadamard_transform.cpp
+++ csrc/fast_hadamard_transform.cpp
@@ -40,6 +40,12 @@ void fast_hadamard_transform_28N_cuda(HadamardParamsBase &params, cudaStream_t s
 template<typename input_t>
 void fast_hadamard_transform_40N_cuda(HadamardParamsBase &params, cudaStream_t stream);
 
+template<typename input_t>
+void fast_hadamard_transform_84N_cuda(HadamardParamsBase &params, cudaStream_t stream);
+
+template<typename input_t>
+void fast_hadamard_transform_172N_cuda(HadamardParamsBase &params, cudaStream_t stream);
+
 void set_hadamard_params(HadamardParamsBase &params,
                          // sizes
                          const size_t batch,
@@ -221,8 +227,7 @@ fast_hadamard_transform_28N(at::Tensor &x, float scale) {
     const int dim = x.size(1);
 
     TORCH_CHECK(dim % (4 * 28) == 0, "fast_hadamard_transform_28N only supports hidden dimension divisible by 112 for now");
-    // TORCH_CHECK(dim <= 28 * 1024, "fast_hadamard_transform_28N only supports hidden dimension at most 28672 for now");
-    TORCH_CHECK(dim <= 28 * 2048, "fast_hadamard_transform_28N only supports hidden dimension at most 28672 for now");
+    TORCH_CHECK(dim <= 28 * 1024, "fast_hadamard_transform_28N only supports hidden dimension at most 28672 for now");
 
     at::Tensor out = torch::empty_like(x);
 
@@ -264,8 +269,8 @@ fast_hadamard_transform_40N(at::Tensor &x, float scale) {
     }
     const int dim = x.size(1);
 
-    TORCH_CHECK(dim % (4 * 40) == 0, "fast_hadamard_transform_40N only supports hidden dimension divisible by 112 for now");
-    TORCH_CHECK(dim <= 40 * 2048, "fast_hadamard_transform_40N only supports hidden dimension at most 4096 for now");
+    TORCH_CHECK(dim % (4 * 40) == 0, "fast_hadamard_transform_40N only supports hidden dimension divisible by 160 for now");
+    TORCH_CHECK(dim <= 40 * 1024, "fast_hadamard_transform_40N only supports hidden dimension at most 40960 for now");
 
     at::Tensor out = torch::empty_like(x);
 
@@ -285,10 +290,54 @@ fast_hadamard_transform_40N(at::Tensor &x, float scale) {
     return out.reshape(shapes_og);
 }
 
+at::Tensor
+fast_hadamard_transform_84N(at::Tensor &x, float scale) {
+    auto input_type = x.scalar_type();
+    TORCH_CHECK(input_type == at::ScalarType::Float || input_type == at::ScalarType::Half || input_type == at::ScalarType::BFloat16);
+
+    TORCH_CHECK(x.is_cuda());
+
+    const auto shapes_og = x.sizes();
+    const int dim_og = x.size(-1);
+    x = x.reshape({-1, dim_og});
+    if (x.stride(-1) != 1) { x = x.contiguous(); }
+    const auto sizes = x.sizes();
+    const int batch_size = sizes[0];
+
+    CHECK_SHAPE(x, batch_size, dim_og);
+    TORCH_CHECK(x.stride(1) == 1);
+
+    if (dim_og % (4 * 84) != 0) {
+        x = torch::nn::functional::pad(x, torch::nn::functional::PadFuncOptions({0, (4 * 84) - dim_og % (4 * 84)}));
+    }
+    const int dim = x.size(1);
+
+    TORCH_CHECK(dim % (4 * 84) == 0, "fast_hadamard_transform_84N only supports hidden dimension divisible by 336 for now");
+    TORCH_CHECK(dim <= 84 * 1024, "fast_hadamard_transform_84N only supports hidden dimension at most 86016 for now");
+
+    at::Tensor out = torch::empty_like(x);
+
+    HadamardParamsBase params;
+    set_hadamard_params(params, batch_size, dim, 84, x, out, scale);
+
+    // Otherwise the kernel will be launched from cuda:0 device
+    // Cast to char to avoid compiler warning about narrowing
+    at::cuda::CUDAGuard device_guard{(char)x.get_device()};
+    auto stream = at::cuda::getCurrentCUDAStream().stream();
+    DISPATCH_ITYPE_FLOAT_AND_HALF_AND_BF16(x.scalar_type(), "fast_hadamard_transform", [&] {
+        fast_hadamard_transform_84N_cuda<input_t>(params, stream);
+    });
+    if (dim_og % (8 * 84) != 0) {
+        out = out.index({torch::indexing::Slice(), torch::indexing::Slice(torch::indexing::None, dim_og)});
+    }
+    return out.reshape(shapes_og);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("fast_hadamard_transform", &fast_hadamard_transform, "Fast Hadamard transform");
     m.def("fast_hadamard_transform_12N", &fast_hadamard_transform_12N, "Fast Hadamard transform with dimension = 12 * power of 2");
     m.def("fast_hadamard_transform_20N", &fast_hadamard_transform_20N, "Fast Hadamard transform with dimension = 20 * power of 2");
     m.def("fast_hadamard_transform_28N", &fast_hadamard_transform_28N, "Fast Hadamard transform with dimension = 28 * power of 2");
     m.def("fast_hadamard_transform_40N", &fast_hadamard_transform_40N, "Fast Hadamard transform with dimension = 40 * power of 2");
+    m.def("fast_hadamard_transform_84N", &fast_hadamard_transform_84N, "Fast Hadamard transform with dimension = 84 * power of 2");
 }
--- fast_hadamard_transform/fast_hadamard_transform_interface.py
+++ fast_hadamard_transform/fast_hadamard_transform_interface.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023, Tri Dao.
-
+import numpy as np
 import math
 try:
     from scipy.linalg import hadamard
@@ -153,6 +153,46 @@ def hadamard_transform_40N(x, scale=1.0):
     """
     return HadamardTransform40NFn.apply(x, scale)
 
+class HadamardTransform84NFn(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, scale=1.0):
+        ctx._hadamard_transform_scale = scale
+        return fast_hadamard_transform_cuda.fast_hadamard_transform_84N(x, scale)
+
+    @staticmethod
+    def backward(ctx, dout):
+        # The Hadamard transform matrix is symmetric, so in the backward pass we multiply by its
+        # transpose, which is itself.
+        return fast_hadamard_transform_cuda.fast_hadamard_transform_84N(dout, ctx._hadamard_transform_scale), None
+
+def hadamard_transform_84N(x, scale=1.0):
+    """
+    Arguments:
+        x: (..., dim)
+        scale: float. Multiply the output by this number.
+    Returns:
+        out: (..., dim)
+
+    Multiply each row of x by the Hadamard transform matrix, where dim = 40 * power of 2.
+    If dim is not 84 * a power of 2, we implicitly pad x with zero so that dim is 84 * the next power of 2.
+    """
+    return HadamardTransform84NFn.apply(x, scale)
+
+
+from csrc.code_gen import had_12_paley
+
+def parse_hadamard(s):
+    lines = s.strip().split('\n')
+    matrix = []
+    for line in lines:
+        row = [1 if c == '+' else -1 for c in line.strip()]
+        matrix.append(row)
+    return np.array(matrix, dtype=int)
+
+def is_pow2(n):
+    return (n & (n - 1) == 0) and (n > 0)
+
 def hadamard_transform_ref(x, scale=1.0):
     """
     x: (..., dim)
@@ -163,10 +203,20 @@ def hadamard_transform_ref(x, scale=1.0):
     x_shape = x.shape
     dim = x.shape[-1]
     x = x.reshape(-1, dim)
-    log_dim = math.ceil(math.log2(dim))
-    dim_padded = 2 ** log_dim
-    if dim != dim_padded:
-        x = F.pad(x, (0, dim_padded - dim))
-    out = F.linear(x, torch.tensor(hadamard(dim_padded, dtype=float), dtype=x.dtype, device=x.device))
-    out = out * scale
+
+    if dim % 12 == 0:
+        assert (is_pow2(dim // 12))
+        H12 = parse_hadamard(had_12_paley)
+        N = dim // 12
+        H12_large = np.kron(np.eye(N, dtype=int), H12)
+        out = F.linear(x, torch.tensor(H12_large, dtype=x.dtype, device=x.device))
+        out = out * scale
+    else:
+        assert (is_pow2(dim))
+        log_dim = math.ceil(math.log2(dim))
+        dim_padded = 2 ** log_dim
+        if dim != dim_padded:
+            x = F.pad(x, (0, dim_padded - dim))
+        out = F.linear(x, torch.tensor(hadamard(dim_padded, dtype=float), dtype=x.dtype, device=x.device))
+        out = out * scale
     return out[..., :dim].reshape(*x_shape)
